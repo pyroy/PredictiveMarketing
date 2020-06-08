a83474334facdc6629f58a39517a75a0eebb1099
pyroy <35765207+pyroy@users.noreply.github.com>
2020-06-08 12:03:18 +0200

    Load in datasets

--- data_parser.py
+++ data_parser.py
@@ -1,3 +1,5 @@
 import pandas
 
-domein_rapport = pandas.read_csv("Domein rapport - Socialbrothers.nl.csv")
+domein_rapport = pandas.read_csv("Datasets/Domein rapport - Socialbrothers.nl.csv")
+google_ads = pandas.read_csv("Datasets/Google Ads - Zoekwoordrapport.csv")
+rapport = pandas.read_csv("Datasets/Zoekwoordrapport - Stofzuiger.csv")
