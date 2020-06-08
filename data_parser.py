3cc81b54d848b69d37818d0b4829ec4a9f58f310
pyroy <35765207+pyroy@users.noreply.github.com>
2020-06-08 12:20:37 +0200

    Correctly load in files, filter on filled in CTR values

--- data_parser.py
+++ data_parser.py
@@ -1,5 +1,11 @@
 import pandas
 
 domein_rapport = pandas.read_csv("Datasets/Domein rapport - Socialbrothers.nl.csv")
-google_ads = pandas.read_csv("Datasets/Google Ads - Zoekwoordrapport.csv")
+google_ads = pandas.read_csv("Datasets/Google Ads - Zoekwoordrapport.csv", skiprows=2)
 rapport = pandas.read_csv("Datasets/Zoekwoordrapport - Stofzuiger.csv")
+
+print(google_ads.head())
+
+google_ads = google_ads.loc[google_ads['CTR'] != ' --']
+
+print(google_ads.head())
