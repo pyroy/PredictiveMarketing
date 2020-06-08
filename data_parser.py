db1a682559fc87a44c74f5965b29fca6e6214d5f
pyroy <35765207+pyroy@users.noreply.github.com>
2020-06-08 12:26:10 +0200

    Commented code

--- data_parser.py
+++ data_parser.py
@@ -1,11 +1,10 @@
 import pandas
 
+# Load in the csv files
 domein_rapport = pandas.read_csv("Datasets/Domein rapport - Socialbrothers.nl.csv")
 google_ads = pandas.read_csv("Datasets/Google Ads - Zoekwoordrapport.csv", skiprows=2)
 rapport = pandas.read_csv("Datasets/Zoekwoordrapport - Stofzuiger.csv")
 
-print(google_ads.head())
-
+# Filter on rows where the CTR is known, we need it for the profit margin equation.
 google_ads = google_ads.loc[google_ads['CTR'] != ' --']
 
-print(google_ads.head())
