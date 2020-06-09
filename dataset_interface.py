d8651d90f4091800810870ac4427fecc79ce605b
pyroy <35765207+pyroy@users.noreply.github.com>
2020-06-09 12:48:17 +0200

    shitty comments toegevoegd

--- dataset_interface.py
+++ dataset_interface.py
@@ -1,9 +1,11 @@
 import pandas
 import pickle
 
+# Load ion the database
 with open("Datasets/domain_rapport.pydb", "rb") as file:
     DB = pickle.load(file)
 
+# Calculate profit per keyword using the equation in berekening.txt
 def equation(database=DB, keyword='social brothers', conv=0.03, productwaarde=100):
     row = DB.loc[DB['Keyword'] == keyword]
     CTR = int(row['Traffic'])/int(row['Search Volume'])
@@ -12,6 +14,7 @@ def equation(database=DB, keyword='social brothers', conv=0.03, productwaarde=10
     omzet_per_click = CTR * conv * productwaarde
     return omzet_per_click - CPC
 
+# Calculate total CTR
 sv = 0
 tt = 0
 for index, row in DB.iterrows():
