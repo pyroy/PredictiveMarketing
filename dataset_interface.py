edf83cee4c4e56eb3b4d8cc3e3568bc529d2e30a
pyroy <35765207+pyroy@users.noreply.github.com>
2020-06-09 12:45:24 +0200

    equation added

--- dataset_interface.py
+++ dataset_interface.py
@@ -1,8 +1,21 @@
 import pandas
 import pickle
 
-with open("Datasets/google_ads.pydb", "rb") as file:
+with open("Datasets/domain_rapport.pydb", "rb") as file:
     DB = pickle.load(file)
 
-def equation(database=DB):
-    pass
+def equation(database=DB, keyword='social brothers', conv=0.03, productwaarde=100):
+    row = DB.loc[DB['Keyword'] == keyword]
+    CTR = int(row['Traffic'])/int(row['Search Volume'])
+    CPC = float(row['CPC'])
+
+    omzet_per_click = CTR * conv * productwaarde
+    return omzet_per_click - CPC
+
+sv = 0
+tt = 0
+for index, row in DB.iterrows():
+    sv += int(row['Search Volume'])
+    tt += int(row['Traffic'])
+    
+tctr = tt / sv
