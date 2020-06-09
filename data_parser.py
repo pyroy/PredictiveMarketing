9312916f0323bd203a679dac4bdcdfff5eb3b0e4
pyroy <35765207+pyroy@users.noreply.github.com>
2020-06-09 11:35:20 +0200

    pydb

--- data_parser.py
+++ data_parser.py
@@ -1,4 +1,5 @@
 import pandas
+import pickle
 
 # Load in the csv files
 domein_rapport = pandas.read_csv("Datasets/Domein rapport - Socialbrothers.nl.csv")
@@ -7,4 +8,8 @@ rapport = pandas.read_csv("Datasets/Zoekwoordrapport - Stofzuiger.csv")
 
 # Filter on rows where the CTR is known, we need it for the profit margin equation.
 google_ads = google_ads.loc[google_ads['CTR'] != ' --']
+google_ads = google_ads.loc[google_ads['CTR'] != '0,00%']
+google_ads.drop(google_ads.tail(3).index,inplace=True)
 
+with open("Datasets/google_ads.pydb", "wb") as file:
+    pickle.dump(google_ads, file)
