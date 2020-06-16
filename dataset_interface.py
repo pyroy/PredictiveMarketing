5faa2e78fda796e09aad31c750368d787c72ba74
pyroy <35765207+pyroy@users.noreply.github.com>
2020-06-16 12:53:32 +0200

    Update formula

--- dataset_interface.py
+++ dataset_interface.py
@@ -20,6 +20,7 @@ def equation1(database=DB, keyword='social brothers', conv=0.03, productwaarde=1
 def get_suggested_keywords(keyword_rapport=DBZ, budget=500, conv=0.03, avg_product_value=100, cutoff=10):
     omzet_values = {}
     volume_values = {}
+    cd_values = {}
     k = []
     for index, row in DBZ.iterrows():
         # clicks * conversion * product value
@@ -27,20 +28,32 @@ def get_suggested_keywords(keyword_rapport=DBZ, budget=500, conv=0.03, avg_produ
         omzet = min(budget/float(row['CPC (USD)']), row['Volume']) * conv * avg_product_value - budget
         omzet_values[row['Keyword']] = omzet
         volume_values[row['Keyword']] = row['Volume']
+        cd_values[row['Keyword']] = row['Competitive Density']
         k.append(row['Keyword'])
-    suggestions = sorted(k, key = lambda keyword: -omzet_values[keyword])[:cutoff]
+
+    # Only takes CPC into account
+    #suggestions = sorted(k, key = lambda keyword: -omzet_values[keyword])[:cutoff]
+
+    # Takes search volume into account
+    #suggestions = sorted(k, key = lambda keyword: -omzet_values[keyword]*math.log(volume_values[keyword]))[:cutoff]
+
+    # Take SV & Competitive Density into account
+    suggestions = sorted(k, key = lambda keyword: -omzet_values[keyword]*math.log(volume_values[keyword])/cd_values[keyword])[:cutoff]
+    
     total_search_volume = sum([volume_values[k] for k in suggestions])
     total_revenue = sum([omzet_values[k] for k in suggestions])
-    return suggestions, total_search_volume, total_revenue
+    total_competition = sum([-math.log(cd_values[k]) for k in suggestions])
+    return suggestions, total_search_volume, total_revenue, total_competition
 
 def get_suggestions():
     budget = 500
     cutoff = 10
-    sk, tsv, tr = get_suggested_keywords(budget = budget/cutoff)
+    sk, tsv, tr, tc = get_suggested_keywords(budget = budget/cutoff)
     print("Suggested keywords:")
     for k in sk:
         print("- " + k)
     print("\nTotal Search Volume:\t{}".format(tsv))
+    print("Negative Log CD:\t{}".format(round(tc,2)))
     print("\n=====\nBudget:\t\t\t{}$".format(budget))
     print("Total Revenue:\t\t{}$".format(round(tr,2)))
     pm = int(100*tr/budget-100)
