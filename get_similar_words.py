3bb5fef4b445b4eaa290e36cd1a6ba9a29302e45
pyroy <35765207+pyroy@users.noreply.github.com>
2020-06-22 12:38:36 +0200

    More senses

--- get_similar_words.py
+++ get_similar_words.py
@@ -4,7 +4,7 @@ instance = Wn_grid_parser(Wn_grid_parser.odwn)
 def get_similar_words(woord):
     ret = []
     senses = instance.lemma_get_generator(woord)
-    for sense in senses[:2]:
+    for sense in senses:
         le_el = instance.les_find_le(sense.get_id())
         if not le_el: raise Exception
         meanings = instance.les_all_les_of_one_synset(le_el.get_synset_id())
