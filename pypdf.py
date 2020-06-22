231251b5ac1fa30c088802a5abc1706c125bee95
mmlatif <murtazablue@hotmail.com>
2020-06-22 14:27:05 +0200

    Update pypdf.py

--- pypdf.py
+++ pypdf.py
@@ -1,4 +1,6 @@
-import pydf
-pdf = pydf.generate_pdf('<h1>this is html</h1>')
-with open('test_doc.pdf', 'wb') as f:
-    f.write(pdf)
+from fpdf import FPDF
+pdf = FPDF()
+pdf.add_page()
+pdf.set_font("Arial", size=10)
+pdf.cell(100,100, txt= "Social Brothers Advice Report", ln=1, align = "C")
+pdf.output("Advice.pdf")
