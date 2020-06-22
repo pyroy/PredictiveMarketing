be43727f75edd4f77e50e0c7e6eb972c239e7783
pyroy <35765207+pyroy@users.noreply.github.com>
2020-06-22 12:27:09 +0200

    SEO and SEA done :D

--- dataset_interface.py
+++ dataset_interface.py
@@ -17,19 +17,21 @@ def equation1(database=DB, keyword='social brothers', conv=0.03, productwaarde=1
     omzet_per_click = CTR * conv * productwaarde
     return omzet_per_click - CPC
 
-def get_suggested_keywords(keyword_rapport=DBZ, conv=0.03, avg_product_value=100, sv_cutoff=100):
+def get_suggested_keywords(keyword_rapport=DBZ, conv=0.03, avg_product_value=100, sv_cutoff=100, ret_cutoff=10):
     profit_values = {}
     volume_values = {}
     keydif_values = {}
     recommend_sea = []
     recommend_seo = []
+    k = []
     for index, row in DBZ.iterrows():
         # clicks * conversion * product value
         if float(row['CPC (USD)']) == 0.0: continue
-        if int(row['Search Volume']) < sv_cutoff: continue
+        if int(row['Volume']) < sv_cutoff: continue
+        if int(row['Number of Results']) == 0: continue
         profit = avg_product_value - int(1/conv) * float(row['CPC (USD)'])
         profit_values[row['Keyword']] = profit
-        volume_values[row['Keyword']] = int(row['Search Volume'])
+        volume_values[row['Keyword']] = int(row['Volume'])
         keydif_values[row['Keyword']] = float(row['Keyword Difficulty'])
         k.append(row['Keyword'])
 
@@ -39,8 +41,9 @@ def get_suggested_keywords(keyword_rapport=DBZ, conv=0.03, avg_product_value=100
     # Takes search volume into account
     sea_suggestions = sorted(k, key = lambda keyword: -profit_values[keyword]*math.log(volume_values[keyword]))
     seo_suggestions = sorted(k, key = lambda keyword: keydif_values[keyword]*math.log(volume_values[keyword]))
+    seo_suggestions = [key_k for key_k in k if keydif_values[key_k] < 80]
 
     # Take SV & Competitive Density into account
     # sea_suggestions = sorted(k, key = lambda keyword: -profit_values[keyword]*math.log(volume_values[keyword])/cd_values[keyword])[:cutoff]
     
-    return sea_suggestions, seo_suggestions
+    return sea_suggestions[:ret_cutoff], seo_suggestions[:ret_cutoff]
