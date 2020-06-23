cd1656f47ecf73dcb31e66c52677ac382e05e13f
mmlatif <murtazablue@hotmail.com>
2020-06-23 11:55:53 +0200

    Delete pypdf.py

--- pypdf.py
+++
@@ -1,6 +0,0 @@
-from fpdf import FPDF
-pdf = FPDF()
-pdf.add_page()
-pdf.set_font("Arial", size=10)
-pdf.cell(100,100, txt= "Social Brothers Advice Report", ln=1, align = "C")
-pdf.output("Advice.pdf")
