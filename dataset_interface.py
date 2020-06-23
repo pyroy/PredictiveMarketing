e5b687442aa4b9dcad6d4875f0ff24a2c02d73f5
pyroy <35765207+pyroy@users.noreply.github.com>
2020-06-23 16:00:36 +0200

    API Call No test yet :(

--- dataset_interface.py
+++ dataset_interface.py
@@ -1,13 +1,15 @@
 import pandas
 import pickle, math
+from io import StringIO
+
+KEY = "key"
+
+def call_api_related_kw(keyword):
+    response = requests.get(f"https://api.semrush.com/?type=phrase_related&key={key}&phrase=seo&export_columns=Ph,Nq,Cp,Co,Nr,Td,Rr,Fk&database=nl&display_limit=10&display_sort=nq_desc&display_filter=%2B|Nq|Lt|1000")
+    DB = pandas.read_csv(StringIO(response.text))
+    return DB
 
 # Load in the database
-with open("Datasets/domain_rapport.pydb", "rb") as file:
-    DB = pickle.load(file)
-
-with open("Datasets/stofzuiger.pydb", "rb") as file:
-    DBZ = pickle.load(file)
-
 def get_suggested_keywords(keyword_rapport=DBZ, conv=0.03, avg_product_value=100, sv_cutoff=100, ret_cutoff=10):
     profit_values = {}
     volume_values = {}
